st3v/fargo

commit 0331c1f524d91c16de1f60cdccfe7b172247ed2c

change PollInterval type to time.Duration

connection.go
@@ -64,7 +64,7 @@ func NewConnFromConfig(conf Config) (c EurekaConnection) {
 		c.ServiceUrls = []string{conf.Eureka.ServerDNSName}
 	}
 	c.Timeout = conf.Eureka.ConnectTimeoutSeconds
-	c.PollInterval = conf.Eureka.PollIntervalSeconds
+	c.PollInterval = time.Duration(conf.Eureka.PollIntervalSeconds) * time.Second
 	c.PreferSameZone = conf.Eureka.PreferSameZone
 	return c
 }

struct.go
@@ -25,6 +25,10 @@ package fargo
  * IN THE SOFTWARE.
  */
 
+import (
+	"time"
+)
+
 // EurekaUrlSlugs is a map of resource names -> eureka URLs
 var EurekaURLSlugs = map[string]string{
 	"Apps":      "apps",
@@ -35,7 +39,7 @@ var EurekaURLSlugs = map[string]string{
 type EurekaConnection struct {
 	ServiceUrls    []string
 	Timeout        int
-	PollInterval   int
+	PollInterval   time.Duration
 	PreferSameZone bool
 	Retries        int
 }

commit e3e751ec2b36ffcad6bde527ed38a78de0c8d779

Use correct poll interval
EurekaConnection.Pollinterval is already a time.Duration, so there's no
need to cast and multiple it by time.Second. This multiplication increases the
interval by a factor of 1000. So if the caller meant to set the interval to 1
second, the code will actually wait for 1000 seconds for each poll.

It looks like this bug was introduced by https://github.com/hudl/fargo/commit/0331c1f

connection.go
@@ -90,7 +90,7 @@ func (e *EurekaConnection) UpdateApp(app *Application) {
 			if err != nil {
 				log.Errorf("Failure updating %s in goroutine", app.Name)
 			}
-			<-time.After(time.Duration(e.PollInterval) * time.Second)
+			<-time.After(e.PollInterval)
 		}
 	}()
 }
@@ -138,7 +138,7 @@ func (e *EurekaConnection) ScheduleAppUpdates(name string, await bool, done <-ch
 	if await {
 		c <- produce()
 	}
-	go sendAppUpdatesEvery(time.Duration(e.PollInterval)*time.Second, produce, c, done)
+	go sendAppUpdatesEvery(e.PollInterval, produce, c, done)
 	return c
 }
 